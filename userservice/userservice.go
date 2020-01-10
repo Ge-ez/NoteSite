ff526a749d366b633866d6dadd8bf450c9597ad3
Sinishaw Tadele <45556713+SineX-Selama@users.noreply.github.com>
2020-01-10 23:03:39 +0300

    Update userservice.go
    Sign up service

--- userservice/userservice.go
+++ userservice/userservice.go
@@ -1,1 +1,14 @@
 package userservice
+
+import (
+  "github.com/notesite/models"
+  "github.com/notesite/user"
+)
+
+func Signup(user *models.User, repo user.UserRepository) error {
+  err := repo.Save(user)
+  if err != nil {
+    return err
+  }
+  return nil
+}
