8b57dbdff4e9b66a60b3f2002d7edc8b52e3f5c1
Sinishaw Tadele <45556713+SineX-Selama@users.noreply.github.com>
2020-01-10 22:58:03 +0300

    Update service.go
    Implemented Save User Interface

--- user/service.go
+++ user/service.go
@@ -1,1 +1,34 @@
 package user
+
+import (
+  "fmt"
+  "database/sql"
+  "github.com/notesite/models"
+  "time"
+)
+
+type UserRepositoryPostgres struct {
+  db *sql.DB
+}
+
+func NewUser() *models.User {
+  return &models.User{
+    Joindate: time.Now(),
+  }
+}
+
+func NewUserRepositoryPostgres(db *sql.DB) *UserRepositoryPostgres {
+  return &UserRepositoryPostgres{db}
+}
+
+func (user *UserRepositoryPostgres) Save(signup *models.User) error {
+
+  _, err := user.db.Exec("INSERT INTO users(name,username,email,password,gender,role,course,token,joindate) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9)",signup.Name, signup.Username, signup.Email, signup.Password, signup.Gender, signup.Role, signup.Course, "", signup.Joindate)
+
+  if err != nil {
+    return err
+    fmt.Println("unable to insert the data")
+  }
+  return nil
+
+}
