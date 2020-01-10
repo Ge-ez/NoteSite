46a6a636735bec1ab0622d90a8fd1b4fa87e7ed6
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-10 22:12:58 +0300

    Created Database Connection

--- Database/conn.go
+++ Database/conn.go
@@ -1,1 +1,20 @@
-package Database
+package database
+
+import (
+  "database/sql"
+  "fmt"
+  _ "github.com/lib/pq"
+  "log"
+)
+
+func Conn() *sql.DB {
+  connstr := "user=postgres dbname=notesite host=localhost password=anteneh23 sslmode=disable"
+  db, err := sql.Open("postgres", connstr)
+  if err != nil {
+    log.Fatal(err)
+    return nil
+  }
+  defer db.Close()
+  fmt.Println("connected")
+  return db
+}
