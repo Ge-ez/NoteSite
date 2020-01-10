584acab5a02b8d3758b17d013eddc104ba10eba1
benyamz01 <58995980+benyamz01@users.noreply.github.com>
2020-01-10 23:52:07 +0300

    Update userRepository.go
    update interface

--- user/userRepository.go
+++ user/userRepository.go
@@ -7,6 +7,7 @@ import (
 type UserRepository interface {
   Save(*models.User) error
   Login(username string) (*models.Usercheck, error)
+  UpdateUser(id string,*models.User) error
+
 }
  
-UpdateUser(id string,*models.User) error
