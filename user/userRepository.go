fe30f06a1290b3e170002b36f88c2d48862565e3
benyamz01 <58995980+benyamz01@users.noreply.github.com>
2020-01-11 11:43:44 +0300

    Update userRepository.go

--- user/userRepository.go
+++ user/userRepository.go
@@ -6,7 +6,7 @@ import (
 
 type UserRepository interface {
   Save(*models.User) error
-  findbyusername(username string) (*models.Usercheck, error)
+  Findbyusername(username string) (*models.Usercheck, error)
   Update(id uint32,*models.User) error
 
 }
