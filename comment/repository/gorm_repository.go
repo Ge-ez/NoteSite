5bdf5d160524eca4790efdd4b0b30129227f34e6
Sinishaw Tadele <45556713+SineX-Selama@users.noreply.github.com>
2020-01-12 17:15:23 +0300

    Update gorm_repository.go
    implemented update comments for user interface

--- comment/repository/gorm_repository.go
+++ comment/repository/gorm_repository.go
@@ -26,3 +26,24 @@ func (commtRepo *CommentGormRepo) Comments() ([]models.Comment, []error) {
 	}
 	return ans, errs
 }
+
+//Comment returns a user Comment stored in the database which has the given id
+func (commtRepo *CommentGormRepo) Comment(id uint) (*models.Comment, []error) {
+	qstn := models.Comment{}
+	errs := commtRepo.conn.Where("id = ?", id).First(&qstn).GetErrors()
+	// errs := commtRepo.conn.First(&qstn, id).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return &qstn, errs
+}
+
+//UpdateComment updates a given Comment in the database
+func (commtRepo *CommentGormRepo) UpdateComment(comment *models.Comment) (*models.Comment, []error) {
+	commt := comment
+	errs := commtRepo.conn.Save(commt).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return commt, errs
+}
