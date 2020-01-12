d47b82e29ea77868cbc1f2a3d130a46b69a2d8ec
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-12 17:25:21 +0300

    Update user_postgres.go

--- user/repository/user_postgres.go
+++ user/repository/user_postgres.go
@@ -4,11 +4,4 @@ import (
 )
 
 
-//UserRepository repository(interface) specifies User user related database operations
-type UserRepository interface {
-	RegisterUser(user *models.User)(*models.User,error)
-	AuthenticateUser(userName string, password string) (*models.Usercheck, error)
-	GetUser(userName string) (*models.User, error)
-	EditUser(user *models.User)(*models.User,[]error)
-	DeleteUser(id uint)(*models.User,error)
-}
+
