5a768d06fad5b0559f54fdd11eddcbf380886970
Sinishaw Tadele <45556713+SineX-Selama@users.noreply.github.com>
2020-01-12 17:09:31 +0300

    Update repository.go
    CommentRepository represents user

--- comment/repository.go
+++ comment/repository.go
@@ -1,1 +1,15 @@
 package comment
+
+import (
+	"github.com/notesite/models"
+)
+
+//CommentRepository represents user - Comment operations
+type CommentRepository interface {
+	Comments() ([]models.Comment, []error)
+	Comment(id uint) (*models.Comment, []error)
+	PostComments(post *models.Post, Comment *models.Comment) ([]models.Comment, []error)
+	UpdateComment(Comment *models.Comment) (*models.Comment, []error)
+	DeleteComment(id uint) (*models.Comment, []error)
+	StoreComment(Comment *models.Comment) (*models.Comment, []error)
+}
