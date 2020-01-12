82e044963cbacb1894616030cebd3a5202298ec4
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-12 17:30:03 +0300

    Update user_service.go
    createtion UserServiceImpl obj

--- user/services/user_service.go
+++ user/services/user_service.go
@@ -1,1 +1,14 @@
 package services
+
+import (
+	"github.com/notesite/entity"
+	"github.com/notesite/user"
+)
+
+type UserServiceImpl struct {
+	userRepo user.UserRepository
+}
+
+func NewUserServiceImpl(UserRep user.UserRepository) *UserServiceImpl {
+	return &UserServiceImpl{userRepo: UserRep}
+}
