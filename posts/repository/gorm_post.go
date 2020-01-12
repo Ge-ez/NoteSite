3fe73e37dce0f2e4cf0e597f12d211970e0f0934
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-12 21:38:08 +0300

    Update gorm_post.go

--- posts/repository/gorm_post.go
+++ posts/repository/gorm_post.go
@@ -5,3 +5,13 @@ import (
 	"github.com/notesite/posts"
 	"github.com/jinzhu/gorm"
 )
+
+//PostGormRepo implements Post.PostRepository interface
+type PostGormRepo struct {
+	conn *gorm.DB
+}
+
+//NewPostGormRepo returns new object of PostGormRepo
+func NewPostGormRepo(db *gorm.DB) posts.PostRepository {
+	return &PostGormRepo{conn: db}
+}
