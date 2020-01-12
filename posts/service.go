8db55151ebfb792e49573cc0b86133379a8d0be9
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-12 21:35:48 +0300

    Update service.go

--- posts/service.go
+++ posts/service.go
@@ -1,1 +1,13 @@
 package posts
+
+import "github.com/notesite/models"
+
+
+//PostService represents user - post operations
+type PostService interface {
+	Posts() ([]models.Post, []error) // gets array of Post struct or returns array of errors
+	GetPost(id uint) (*models.Post, []error)
+	UpdatePost(post *models.Post) (*models.Post, []error)
+	DeletePost(id uint) (*models.Post, []error)
+	StorePost(post *models.Post) (*models.Post, []error)
+}
