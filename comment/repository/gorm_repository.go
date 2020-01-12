41700e41660f232e803b6100672e6090ce0c0655
Sinishaw Tadele <45556713+SineX-Selama@users.noreply.github.com>
2020-01-12 17:13:30 +0300

    Update gorm_repository.go
    implemented comments method for user repository interface

--- comment/repository/gorm_repository.go
+++ comment/repository/gorm_repository.go
@@ -1,1 +1,28 @@
 package repository
+
+import (
+	"github.com/notesite/comment"
+	"github.com/notesite/models"
+	"github.com/jinzhu/gorm"
+)
+
+//CommentGormRepo implements comment.CommentRepository interface
+type CommentGormRepo struct {
+	conn *gorm.DB
+}
+
+//NewCommentGormRepo returns new object of CommentGormRepo
+func NewCommentGormRepo(db *gorm.DB) comment.CommentRepository {
+	return &CommentGormRepo{conn: db}
+}
+
+//comments returns all user comments stored in the database
+func (commtRepo *CommentGormRepo) Comments() ([]models.Comment, []error) {
+	ans := []models.Comment{}
+	errs := commtRepo.conn.Find(&ans).GetErrors()
+	if len(errs) > 0 {
+		
+		return nil, errs
+	}
+	return ans, errs
+}
