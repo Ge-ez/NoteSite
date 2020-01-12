0c15dea9c42e909a6e57f7c6da0a715cbe0de475
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-12 17:26:15 +0300

    Update repository.go
    UserRepository repository(interface) specifies User user related database operations

--- user/repository.go
+++ user/repository.go
@@ -1,1 +1,15 @@
 package user
+
+import (
+	"github.com/notesite/models"
+)
+
+
+//UserRepository repository(interface) specifies User user related database operations
+type UserRepository interface {
+	RegisterUser(user *models.User)(*models.User,error)
+	AuthenticateUser(userName string, password string) (*models.Usercheck, error)
+	GetUser(userName string) (*models.User, error)
+	EditUser(user *models.User)(*models.User,[]error)
+	DeleteUser(id uint)(*models.User,error)
+}
