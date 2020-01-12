9655812c04a13eec8666f8fbff46015270e90228
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-12 21:35:05 +0300

    Update repository.go

--- posts/repository.go
+++ posts/repository.go
@@ -1,1 +1,13 @@
 package posts
+
+import "github.com/notesite/models"
+
+
+//PostRepository represents post related operations on the database
+type PostRepository interface {
+	Posts() ([]models.Post, []error)
+	GetPost(id uint) (*models.Post, []error)
+	UpdatePost(post *models.Post) (*models.Post, []error)
+	DeletePost(id uint) (*models.Post, []error)
+	StorePost(post *models.Post) (*models.Post,[]error)
+}
