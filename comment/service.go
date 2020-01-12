072d3b81970d9be9afda40a2fb7120e512f3bb8a
Sinishaw Tadele <45556713+SineX-Selama@users.noreply.github.com>
2020-01-12 17:10:57 +0300

    Update service.go
    CommentService represents user

--- comment/service.go
+++ comment/service.go
@@ -1,1 +1,15 @@
 package comment
+
+import (
+	"github.com/notesite/models"
+)
+
+//CommentService represents user - Comment operations
+type CommentService interface {
+	Comments() ([]models.Comment, []error)
+	Comment(id uint) (*models.Comment, []error)
+	PostComments(post *models.Post, comment *models.Comment) ([]models.Comment, []error)
+	UpdateComment(comment *models.Comment) (*models.Comment, []error)
+	DeleteComment(id uint) (*models.Comment, []error)
+	StoreComment(comment *models.Comment) (*models.Comment, []error)
+}
