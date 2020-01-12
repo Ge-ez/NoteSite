0272f6b185cdadb102aa03205375979abe4ffe06
Sinishaw Tadele <45556713+SineX-Selama@users.noreply.github.com>
2020-01-12 17:20:51 +0300

    Update comment_service.go
    implemented comments method for user service  interface

--- comment/service/comment_service.go
+++ comment/service/comment_service.go
@@ -1,1 +1,34 @@
 package service
+
+import (
+	"github.com/notesite/comment"
+	"github.com/notesite/models"
+)
+
+//commentService implements comment.Service
+type CommentService struct {
+	commentRepo comment.CommentRepository
+}
+
+// NewCommentService returns a new CommentService object
+func NewCommentService(ansRepo comment.CommentRepository) comment.CommentService {
+	return &CommentService{commentRepo: ansRepo}
+}
+
+//comments returns all stored comments
+func (as *CommentService) Comments() ([]models.Comment, []error) {
+	commts, errs := as.commentRepo.Comments()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return commts, errs
+}
+
+//Comment retrieves stored Comment given its id
+func (as *CommentService) Comment(id uint) (*models.Comment, []error) {
+	commt, errs := as.commentRepo.Comment(id)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return commt, errs
+}
