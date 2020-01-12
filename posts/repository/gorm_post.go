21be9c4ef5b1dbcc86ce86ed01399f42eba18942
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-12 21:38:31 +0300

    Update gorm_post.go

--- posts/repository/gorm_post.go
+++ posts/repository/gorm_post.go
@@ -15,3 +15,13 @@ type PostGormRepo struct {
 func NewPostGormRepo(db *gorm.DB) posts.PostRepository {
 	return &PostGormRepo{conn: db}
 }
+
+//Posts returns all user Posts stored in the database
+func (postRepo *PostGormRepo) Posts() ([]models.Post, []error) {
+	posts := []models.Post{}
+	errs := postRepo.conn.Find(&posts).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return posts, errs
+}
