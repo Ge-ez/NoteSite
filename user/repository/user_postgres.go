c64e26c93ede6c86c6821e451153a0f3de60d3de
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-12 17:37:02 +0300

    Update user_postgres.go
    register user

--- user/repository/user_postgres.go
+++ user/repository/user_postgres.go
@@ -17,3 +17,32 @@ func NewUserRepositoryImpl(Conn *gorm.DB) *UserRepositoryImpl {
 }
 
 
+func (uri *UserRepositoryImpl) RegisterUser(user *models.User)(*models.User ,error) {
+	users := user
+//username taken?
+_,err := uri.conn.Raw("SELECT * FROM users WHERE username = ?",user.Username).Rows()
+if err != nil{
+	return nil,errors.New("user name already taken try other")
+}
+errs := uri.conn.Create(users).GetErrors()
+if len(errs) > 0 {
+	return nil, errors.New("selection has failed")
+}
+return users,nil
+
+	// //username taken?
+	// _,err := uri.conn.Query("SELECT * FROM users where username = $1", user.UserName)
+	// if err != nil {
+	// 	//fmt.Println("here is the problem")
+	// 	//panic(err)
+	// 	return errors.New("user name already taken try other")
+	// }
+	// _, err = uri.conn.Exec("INSERT INTO users (username,first_name,last_name,email,password,phone,image) VALUES ($1,$2,$3,$4,$5,$6,$7)", user.UserName, user.FirstName,user.LastName, user.Email,user.Password, user.Phone, user.Image)
+
+	// if err != nil {
+	// 	fmt.Println("check me")
+	// 	return errors.New("insertion has failed")
+	// }
+	// return nil
+
+}
