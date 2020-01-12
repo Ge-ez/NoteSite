812310d01e41d7cb84acdc94db04e5d1533baecd
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-12 17:27:38 +0300

    Update services.go
    this is our service usescase(has (interface)abstract classes that outer layers can use)

--- user/services.go
+++ user/services.go
@@ -1,2 +1,15 @@
 package user
 
+import (
+	"github.com/notesite/models"
+)
+
+
+//UserService ... this is our service usescase(has (interface)abstract classes that outer layers can use)
+type UserService interface {
+	RegisterUser(user *models.User)(*models.User,error)
+	AuthenticateUser(userName string, password string) (*models.Usercheck, error)
+	GetUser(userName string) (*models.User, error)
+	EditUser(user *models.User)(*models.User,[]error)
+	DeleteUser(id uint)(*models.User,error)
+}
