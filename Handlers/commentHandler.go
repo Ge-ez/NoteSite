fc8c198b8b85190e73598e2b903604dd3d30d41b
Sinishaw Tadele <45556713+SineX-Selama@users.noreply.github.com>
2020-01-12 17:53:20 +0300

    Update commentHandler.go
    Putcomment handles PUT comments/:id request

--- Handlers/commentHandler.go
+++ Handlers/commentHandler.go
@@ -105,3 +105,50 @@ func (ah *CommentHandler) PostComment(w http.ResponseWriter, r *http.Request, _ 
 	http.Error(w, http.StatusText(http.StatusCreated), http.StatusCreated)
 	return
 }
+//Putcomment handles PUT comments/:id request
+func (ah *CommentHandler) PutComment(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id, err := strconv.Atoi(ps.ByName("id"))
+
+         	if err != nil {
+         		w.Header().Set("Content-Type", "application/json")
+         		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+         		return
+         	}
+
+	comment, errs := ah.commentService.Comment(uint(id))
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	l := r.ContentLength
+
+	body := make([]byte, l)
+
+	r.Body.Read(body)
+
+	json.Unmarshal(body, &comment)
+
+	comment, errs = ah.commentService.UpdateComment(comment)
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	output, err := json.MarshalIndent(comment, "", "\t")
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+
+}
