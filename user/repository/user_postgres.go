445ed460181182cef612ccaf158eb41a6ca57788
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-12 17:35:02 +0300

    Update user_postgres.go
     create an object of  UserReposiotryImpl

--- user/repository/user_postgres.go
+++ user/repository/user_postgres.go
@@ -1,7 +1,19 @@
 package repository
+
 import (
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+	"errors"
+
 	"github.com/notesite/models"
 )
 
+type UserRepositoryImpl struct {
+	conn *gorm.DB
+}
+
+func NewUserRepositoryImpl(Conn *gorm.DB) *UserRepositoryImpl {
+	return &UserRepositoryImpl{conn: Conn}
+}
 
 
