cac297e855d0435a9e4af052d893d71ff2c440c2
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-12 17:42:34 +0300

    Update userHandler.go
    handled get user and validate login session

--- Handlers/userHandler.go
+++ Handlers/userHandler.go
@@ -28,3 +28,36 @@ var dbSessions = map[string]string{} //session ID,user ID
 func NewUserHandler(T *template.Template, US user.UserService) *UserHandler {
 	return &UserHandler{tmpl: T, userSrv: US}
 }
+func (uh *UserHandler) alreadyLoggedIn(req *http.Request) bool {
+	c, err := req.Cookie("session")
+	if err != nil {
+		return false
+	}
+	un := dbSessions[c.Value]
+	_, errr := uh.userSrv.GetUser(un)
+	if errr != nil {
+		return false
+	}
+	return true
+
+}
+func (uh *UserHandler) getUser(w http.ResponseWriter, req *http.Request) *models.User {
+	//get cookie
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID, _ := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+	}
+	http.SetCookie(w, c)
+
+	//if the user exists already,get user
+	var u *models.User
+	if un, ok := dbSessions[c.Value]; ok {
+		u, _ = uh.userSrv.GetUser(un)
+	}
+	return u
+}
+
