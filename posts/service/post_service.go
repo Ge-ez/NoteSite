94e45ebe5b70937a751e7f23ea3e34c3b0a02719
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-12 21:43:42 +0300

    Update post_service.go

--- posts/service/post_service.go
+++ posts/service/post_service.go
@@ -3,3 +3,13 @@ import (
 	"github.com/notesite/models"
 	"github.com/notesite/posts"
 )
+
+//PostService implements post.Service
+type PostService struct {
+	postRep posts.PostRepository
+}
+
+// NewPostService returns a new PostService object
+func NewPostService(postsRepo posts.PostRepository) posts.PostService {
+	return &PostService{postRep: postsRepo}
+}
