d9b3c64c1b64f46d8a183898b4d92572a1a7f08d
natysho <59202340+natysho@users.noreply.github.com>
2020-01-27 12:22:11 -0800

    Update random.go

--- security/token/random.go
+++ security/token/random.go
@@ -6,3 +6,14 @@ import (
 	mrand "math/rand"
 	"time"
 )
+// GenerateRandomBytes returns securely generated random bytes.
+func GenerateRandomBytes(n int) ([]byte, error) {
+	mrand.Seed(time.Now().UnixNano())
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
