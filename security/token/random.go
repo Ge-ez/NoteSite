e94d768469409c75d1e0682b432c670a2bf061a7
natysho <59202340+natysho@users.noreply.github.com>
2020-01-27 12:23:39 -0800

    Update random.go

--- security/token/random.go
+++ security/token/random.go
@@ -21,5 +21,16 @@ func GenerateRandomString(s int) (string, error) {
 	b, err := GenerateRandomBytes(s)
 	return base64.URLEncoding.EncodeToString(b), err
 }
+// GenerateRandomID generates random id for a session
+func GenerateRandomID(s int) string {
+	mrand.Seed(time.Now().UnixNano())
+
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, s)
+	for i := range b {
+		b[i] = letterBytes[mrand.Int63()%int64(len(letterBytes))]
+	}
+	return string(b)
+}
 
 
