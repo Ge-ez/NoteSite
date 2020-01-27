d3b43b3f8d75770c9f7677012348e8bbf1bc4c6a
natysho <59202340+natysho@users.noreply.github.com>
2020-01-27 12:08:17 -0800

    Update session.go

--- security/session/session.go
+++ security/session/session.go
@@ -8,3 +8,17 @@ import (
 	"github.com/notesite/security/token"
 	"github.com/dgrijalva/jwt-go"
 )
+func Create(claims jwt.Claims, sessionID string, signingKey []byte, w http.ResponseWriter) {
+
+	signedString, err := token.Generate(signingKey, claims)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	c := http.Cookie{
+		Name:     sessionID,
+		Value:    signedString,
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &c)
+}
