2411416120f5d1922036ff56de4f77918d1cee41
benyamz01 <58995980+benyamz01@users.noreply.github.com>
2020-01-27 18:35:42 +0300

    Update course_service.go

--- course/service/course_service.go
+++ course/service/course_service.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"github.com/notesite/models"
+	"github.com/notesite/course"
+)
+
+type CourseServiceImpl struct {
+	courseRepo course.CourseRepository
+}
+
+// NewCourseServiceImpl  returns new CourseServiceImpl
+func NewCourseServiceImpl(CourseRepo course.CourseRepository) course.CourseService {
+	return &CourseServiceImpl{courseRepo: CourseRepo}
+}
