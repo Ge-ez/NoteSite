dd673036e0e7835239971dc799d9090f6da330cf
benyamz01 <58995980+benyamz01@users.noreply.github.com>
2020-01-27 18:26:03 +0300

    Update course_gorm.go

--- course/repository/course_gorm.go
+++ course/repository/course_gorm.go
@@ -1,1 +1,19 @@
 package repository
+
+import (
+	"github.com/notesite/models"
+	"github.com/notesite/course"
+	"github.com/jinzhu/gorm"
+)
+
+// CourseRepositoryImpl implements the menu.CourseRepository interface
+type CourseRepositoryImpl struct {
+	conn *gorm.DB
+}
+
+// NewCourseRepositoryImpl returns a new a new object of CourseRepositoryImpl
+func NewCourseRepositoryImpl(db *gorm.DB) course.CourseRepository {
+	return &CourseRepositoryImpl{conn: db}
+}
+
+// Courses returns all course Courses stored in the database
