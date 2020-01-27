f50028a1eb1fb1fd12f30c3994df57b236aa9215
benyamz01 <58995980+benyamz01@users.noreply.github.com>
2020-01-27 18:46:09 +0300

    Update repository.go

--- course/repository.go
+++ course/repository.go
@@ -1,1 +1,10 @@
 package course
+import "github.com/notesite/models"
+
+type CourseRepository interface {
+	Courses() ([]models.Course, []error)
+	Course(id uint) (*models.Course, []error)
+	UpdateCourse(course *models.Course) (*models.Course, []error)
+	DeleteCourse(id uint) (*models.Course, []error)
+	StoreCourse(course *models.Course) (*models.Course, []error)
+}
