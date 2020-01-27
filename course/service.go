65dc9ea3cf124bdfad44bd9f50d7ec2ebf812131
benyamz01 <58995980+benyamz01@users.noreply.github.com>
2020-01-27 18:47:33 +0300

    Update service.go

--- course/service.go
+++ course/service.go
@@ -1,1 +1,14 @@
 package course
+package course
+
+import (
+    "github.com/notesite/models"
+)
+
+type CourseService interface{
+    Courses() ([]models.Course, []error)
+   	Course(id uint) (*models.Course, []error)
+   	UpdateCourse(course *models.Course) (*models.Course, []error)
+   	DeleteCourse(id uint) (*models.Course, []error)
+   	StoreCourse(course *models.Course) (*models.Course, []error)
+}
