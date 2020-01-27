cf8eab82d2ade4df7448debd51a2c2f1436eba28
Sinishaw Tadele <45556713+SineX-Selama@users.noreply.github.com>
2020-01-27 23:50:29 +0300

    Update data.go
    email address maching, input values and validations

--- form/data.go
+++ form/data.go
@@ -1,1 +1,18 @@
 package form
+
+import (
+	"fmt"
+	"net/url"
+	"regexp"
+	"unicode/utf8"
+)
+
+// EmailRX represents email address maching pattern
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+// Input represents form input values and validations
+type Input struct {
+	Values  url.Values
+	VErrors ValidationErrors
+	CSRF    string
+}
