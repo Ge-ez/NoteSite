d3795bc579e99b8b6d2a009675584714959d9ca7
Sinishaw Tadele <45556713+SineX-Selama@users.noreply.github.com>
2020-01-27 23:52:21 +0300

    Update permission.go
    authorities

--- security/permission/permission.go
+++ security/permission/permission.go
@@ -1,2 +1,43 @@
 package permission
 
+import (
+	"strings"
+)
+
+type userpermission struct {
+	roles   []string
+	methods []string
+}
+
+type authority map[string]userpermission
+
+var authorities = authority{
+	"/homepage": userpermission{
+		roles:   []string{"student"},
+		methods: []string{"GET", "POST"},
+	},
+	"/posts": userpermission{
+		roles:   []string{"student",},
+		methods: []string{"GET", "POST"},
+	},
+	"/uploadnote": userpermission{
+		roles:   []string{"student","teacher"},
+		methods: []string{"POST"},
+	},
+	"/editprofile": userpermission{
+		roles:   []string{"student"},
+		methods: []string{"POST"},
+	},
+	"/myprofile": userpermission{
+		roles:   []string{"student","teacher"},
+		methods: []string{"GET"},
+	},
+	"/adminhomepage": userpermission{
+		roles:   []string{"admin"},
+		methods: []string{"GET"},
+	},
+	"/logout": userpermission{
+    		roles:   []string{"student","teacher"},
+    		methods: []string{"POST"},
+    	},
+}
