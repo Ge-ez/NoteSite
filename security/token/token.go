575f820257b5cc60506e1826907f7c089b4a519d
natysho <59202340+natysho@users.noreply.github.com>
2020-01-27 12:16:31 -0800

    Update token.go

--- security/token/token.go
+++ security/token/token.go
@@ -7,3 +7,9 @@ type CustomClaims struct {
 	username string `json:"username"`
 	jwt.StandardClaims
 }
+// Generate generates jwt token
+func Generate(signingKey []byte, claims jwt.Claims) (string, error) {
+	tn := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedString, err := tn.SignedString(signingKey)
+	return signedString, err
+}
