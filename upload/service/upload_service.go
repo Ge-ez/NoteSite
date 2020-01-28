2e05c17d05aedd69b6951382ab5d3fa4c4b5f336
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-28 16:32:50 +0300

    Struct and Functions Commited

--- upload/service/upload_service.go
+++ upload/service/upload_service.go
@@ -1,1 +1,29 @@
-packege service
+package service
+
+import(
+    "github.com/notesite/models"
+    "github.com/notesite/upload"
+)
+
+type UploadServiceImpl struct{
+    uploadrepo upload.UploadRepository
+}
+
+func NewUploadSericeImpl(upldServ *UploadServiceImpl) *UploadServiceImpl{
+    return &UploadServiceImpl{uploadrepo:upldServ}
+}
+
+func (usi *UploadServiceImpl) StoreNote(notes *models.Upload) (*models.Upload, []error) {
+	note,err := usi.uploadrepo.StoreNote(notes)
+    	if len(err) > 0 {
+    		return note,err
+    	}
+    	return note,nil
+}
+func (usi *UploadServiceImpl) Notes() (*models.Upload,[]error){
+        notes, err := usi.uploadrepo.Notes()
+        	if len(err) > 0 {
+        		return nil, err
+        	}
+        	return notes, err
+}
