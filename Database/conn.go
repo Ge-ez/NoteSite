cb6566e0bc3dd57bf8d489bf368c69529506088c
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-28 23:15:08 +0300

    Update conn.go

--- Database/conn.go
+++ Database/conn.go
@@ -1,20 +1,20 @@
 package database
 
 import (
-  "database/sql"
-  "fmt"
-  _ "github.com/lib/pq"
-  "log"
+	"fmt"
+	"github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/postgres"
+	"log"
 )
 
-func Conn() *sql.DB {
-  connstr := "user=postgres dbname=notesite host=localhost password=anteneh23 sslmode=disable"
-  db, err := sql.Open("postgres", connstr)
-  if err != nil {
-    log.Fatal(err)
-    return nil
-  }
-  defer db.Close()
-  fmt.Println("connected")
-  return db
+func Conn() *gorm.DB {
+	 connstr := "user=postgres dbname=notesite host=localhost password=anteneh23 sslmode=disable"
+
+	db, err := gorm.Open("postgres", connstr)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	fmt.Println("connected")
+	return db
 }
