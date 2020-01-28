c99686deee7ff4820f6c7896663771bc28f1fd9c
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-28 16:16:41 +0300

    UploadRepositoryImpl Struct Created
    This Struct creates the connection to the database

--- upload/repository/gorm_upload.go
+++ upload/repository/gorm_upload.go
@@ -1,1 +1,11 @@
 package repository
+
+import (
+    "github.com/notesite/models"
+    "github.com/notesite/upload"
+    "github.com/jinzhu/gorm"
+)
+
+type UploadRepositoryImpl struct{
+    conn gorm.DB
+}
