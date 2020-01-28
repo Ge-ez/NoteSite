065e661d75c5534bb0be246446af256a99776aa3
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-28 23:52:02 +0300

    Update repository.go

--- upload/repository.go
+++ upload/repository.go
@@ -3,11 +3,14 @@ package upload
 import "github.com/notesite/models"
 
 type UploadRepository interface{
-     Notes() (*models.Upload,[]error)
+    StoreNote(notes *models.Upload) (*models.Upload, []error)
+     Notes() ([]models.Upload,[]error)
      Note(id uint) (*models.Upload,[]error)
      UpdateNote(note *models.Upload) (*models.Upload,[]error)
      DeleteNote(id uint) (*models.Upload,[]error)
-     NotesByStatus(status string) (*models.Upload,[]error)
-     NotesByUploader(id uint) (*models.Upload,[]error)
-     NotesByCourseName(course string) (*models.Upload,[]error)
+     NotesByStatus(status string) ([]models.Upload,[]error)
+     NotesByUploader(id uint) ([]models.Upload,[]error)
+     NotesByCourseName(course string) ([]models.Upload,[]error)
+     RateArticle(rating *models.Rating)
+     NoteRateCount(FileId uint) int
  }
