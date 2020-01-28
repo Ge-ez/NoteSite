88e8ccc21057b8753e15b8efc5235e61c887149a
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-28 23:50:33 +0300

    Update user_service.go

--- user/services/user_service.go
+++ user/services/user_service.go
@@ -1,53 +1,76 @@
 package services
 
 import (
-	"github.com/notesite/entity"
+	"github.com/notesite/models"
 	"github.com/notesite/user"
 )
 
+//UserServiceImpl implements user.UserService interface
 type UserServiceImpl struct {
 	userRepo user.UserRepository
 }
 
+//NewUserServiceImpl ... creates an object of UserServiceImpl
 func NewUserServiceImpl(UserRep user.UserRepository) *UserServiceImpl {
 	return &UserServiceImpl{userRepo: UserRep}
 }
 
-func (usi *UserServiceImpl) RegisterUser(user *models.User) (*models.User,error) {
-	usr,err := usi.userRepo.RegisterUser(user)
-	if err != nil {
-		return usr,err
+
+func (usi *UserServiceImpl) Users() ([]models.User, []error) {
+	usrs, err := usi.userRepo.Users()
+	if len(err) > 0 {
+		return nil, err
 	}
-	return usr,nil
+	return usrs, err
 }
 
-func (usi *UserServiceImpl) AuthenticateUser(userName string, password string) (*models.Usercheck, error) {
-	user, err := usi.userRepo.AuthenticateUser(userName, password)
-	if err != nil {
-		return user, err
-	}
-	return user, nil
-}
-func (usi *UserServiceImpl) GetUser(userName string) (*models.User, error) {
+//GetUser ... returns one user row with the given username
+func (usi *UserServiceImpl) GetUser(id uint) (*models.User, []error) {
 	//check username?
-	user, err := usi.userRepo.GetUser(userName)
-	if err != nil {
-		return user, err
+	user, err := usi.userRepo.GetUser(id)
+	if len(err) > 0 {
+		return nil, err
 	}
 	return user, nil
+}
 
+func (usi *UserServiceImpl) GetUserByUsername(username string) (*models.User, []error){
+   user, err := usi.userRepo.GetUserByUsername(username)
+   	if len(err) > 0 {
+   		return nil, err
+   	}
+   	return user, err
 }
-func (usi *UserServiceImpl) EditUser(user *models.User)(*models.User,[]error) {
-	urs,err := usi.userRepo.EditUser(user)
-	if err != nil {
-		return urs,err
+
+
+//RegisterUser ... registers a new user
+func (usi *UserServiceImpl) RegisterUser(user *models.User) (*models.User,[]error) {
+	user,err := usi.userRepo.RegisterUser(user)
+	if len(err) > 0 {
+		return user,err
 	}
-	return urs,nil
+	return user,nil
 }
-func (usi *UserServiceImpl) DeleteUser(id uint)(*models.User,error) {
-	urs,err := usi.userRepo.DeleteUser(id)
-	if err != nil {
-		return urs,nil
+//EditUser ... edit existing user data(profile)
+func (usi *UserServiceImpl) EditUser(users *models.User)(*models.User,[]error) {
+	user,err := usi.userRepo.EditUser(users)
+	if len(err) > 0 {
+		return user,err
 	}
-	return urs,nil
+	return user,nil
 }
+
+//DeleteUser ... delete existing user with the given id
+func (usi *UserServiceImpl) DeleteUser(id uint)(*models.User,[]error) {
+	user,err := usi.userRepo.DeleteUser(id)
+	if len(err) > 0 {
+		return nil,err
+	}
+	return user,nil
+}
+
+func (usi *UserServiceImpl) UsernameExists(username string) bool {
+	exists := usi.userRepo.UsernameExists(username)
+	return exists
+}
+
