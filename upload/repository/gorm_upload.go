adf6fa9f4f67fe35e5007b1ef8234fd66082efdf
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-29 00:02:47 +0300

    Update gorm_upload.go

--- upload/repository/gorm_upload.go
+++ upload/repository/gorm_upload.go
@@ -1,10 +1,8 @@
 package repository
 
 import (
-	"fmt"
+	"github.com/jinzhu/gorm"
 	"github.com/notesite/models"
-    "github.com/jinzhu/gorm"
-	"github.com/techreview/entities"
 )
 
 type UploadRepositoryImpl struct{
@@ -86,45 +84,4 @@ func (upld *UploadRepositoryImpl) NotesByCourseName(course string) ([]models.Upl
     		return nil, errs
     	}
     	return note, errs
-}
-
-func (upld *UploadRepositoryImpl) RateNote(rating *models.Rating) {
-
-	errs := upld.conn.Where("article_id = ? AND user_id= ?", rating.FileID, rating.UserID).First(&rating).GetErrors()
-
-	if len(errs) > 0 {
-
-		errs := upld.conn.Create(rating).GetErrors()
-		if len(errs) > 0 {
-			fmt.Println("error storing  ratings")
-			return
-		}
-		fmt.Println("stored   ratings")
-		fmt.Println("query worked")
-		return
-
-	} else{
-		errs := upld.conn.Delete(rating).GetErrors()
-		if len(errs) > 0 {
-			fmt.Println("error while deleting  ratings", errs)
-			return
-		}
-		fmt.Println("deleted answer  ratings successfuly")
-		return
-	}
-}
-
-func (upld *UploadRepositoryImpl)  NoteRateCount(FileId uint) int{
-	var count int
-	var noteRating models.Rating
-	//questionFollows := entities.QuestionFollow{}
-	upld.conn.Model(&noteRating).Where("article_id = ?", FileId).Count(&count)
-	// update the count value in articles table
-	upld.conn.Model(&entities.Article{}).Where("id = ?", FileId).UpdateColumn("number_of_ratings", count)
-	// update the average ratings in articles table
-	upld.conn.Model(&entities.Article{}).Where("id = ?", FileId).UpdateColumn("average_rating", count/5) // it is doing it just the data
-
-	fmt.Println("counted succesfully, with value: ", count)
-
-	return count
 }
