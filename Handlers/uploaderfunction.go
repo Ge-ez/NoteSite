663c36d580de8919ecfca7597a2abbd6cf892784
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-28 14:58:19 +0300

    Function UploadFile created and defined

--- Handlers/uploaderfunction.go
+++ Handlers/uploaderfunction.go
@@ -1,1 +1,23 @@
 package handlers
+
+import(
+    	"io"
+    	"mime/multipart"
+    	"os"
+    	"path/filepath"
+)
+
+func UploadFile(mf *multipart.File, name string) error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	path := filepath.Join(wd, "views", "assets", "files", name)
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	io.Copy(file, *mf)
+	return nil
+}
