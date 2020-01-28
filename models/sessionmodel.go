b9cf862b340d5e90aa32dc594d3f5c6a3cf89906
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-28 23:57:02 +0300

    Delete sessionmodel.go

--- models/sessionmodel.go
+++
@@ -1,7 +0,0 @@
-package models
-type Session struct {
-	ID         uint
-	UUID       string `gorm:"type:varchar(255);not null"`
-	Expires    int64  `gorm:"type:varchar(255);not null"`
-	SigningKey []byte `gorm:"type:varchar(255);not null"`
-}
