21c199ab436814103c28809cf5808af3cad33cac
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-28 23:47:33 +0300

    Update services.go

--- user/services.go
+++ user/services.go
@@ -5,11 +5,22 @@ import (
 )
 
 
-//UserService ...
+//UserService ... this is our service usescase(has (interface)abstract classes that outer layers can use)
 type UserService interface {
-	RegisterUser(user *models.User)(*models.User,error)
-	AuthenticateUser(userName string, password string) (*models.Usercheck, error)
-	GetUser(userName string) (*models.User, error)
+	RegisterUser(user *models.User)(*models.User,[]error)
+	Users() ([]models.User,[]error)
+	GetUser(int uint) (*models.User,[]error)
+    GetUserByUsername(username string) (*models.User, []error)
 	EditUser(user *models.User)(*models.User,[]error)
-	DeleteUser(id uint)(*models.User,error)
+	DeleteUser(id uint)(*models.User,[]error)
+	UsernameExists(username string) bool
+}
+
+
+
+// SessionService specifies logged in user session related service
+type SessionService interface {
+	Session(sessionID string) (*models.Session, []error)
+	StoreSession(session *models.Session) (*models.Session, []error)
+	DeleteSession(sessionID string) (*models.Session, []error)
 }
