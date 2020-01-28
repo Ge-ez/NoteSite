13c068e13f7a1b317cac0d9d62b94b256ab8b48c
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-28 23:52:41 +0300

    Update service.go

--- upload/service.go
+++ upload/service.go
@@ -3,11 +3,13 @@ package upload
 import "github.com/notesite/models"
 
 type UploadService interface{
-     Notes() (*models.Upload,[]error)
+    StoreNote(notes *models.Upload) (*models.Upload, []error)
+     Notes() ([]models.Upload,[]error)
      Note(id uint) (*models.Upload,[]error)
      UpdateNote(note *models.Upload) (*models.Upload,[]error)
      DeleteNote(id uint) (*models.Upload,[]error)
-     NotesByStatus(status string) (*models.Upload,[]error)
-     NotesByUploader(id uint) (*models.Upload,[]error)
-     NotesByCourseName(course string) (*models.Upload,[]error)
+     NotesByStatus(status string) ([]models.Upload,[]error)
+     NotesByUploader(id uint) ([]models.Upload,[]error)
+     NotesByCourseName(course string) ([]models.Upload,[]error)
+
  }
