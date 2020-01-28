e8034352a5caca240b560c722413630080ddc673
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-28 16:18:47 +0300

    Functions Created

--- upload/repository/gorm_upload.go
+++ upload/repository/gorm_upload.go
@@ -9,3 +9,15 @@ import (
 type UploadRepositoryImpl struct{
     conn gorm.DB
 }
+
+func NewUploadRepositoryImpl(db *gorm.DB) *UploadRepositoryImpl{
+    return &UploadRepositoryImpl{conn:db}
+}
+func (upld *UploadRepositoryImpl) StoreNote(notes *models.Upload) (*models.Upload, []error) {
+	note := notes
+	errs := upld.conn.Create(note).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return note, errs
+}
