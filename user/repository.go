36e40c88ba9952b44742e1367b1ad8680c6b91b2
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-28 23:46:59 +0300

    Update repository.go

--- user/repository.go
+++ user/repository.go
@@ -7,9 +7,20 @@ import (
 
 //UserRepository repository(interface) specifies User user related database operations
 type UserRepository interface {
-	RegisterUser(user *models.User)(*models.User,error)
-	AuthenticateUser(userName string, password string) (*models.Usercheck, error)
-	GetUser(userName string) (*models.User, error)
+    Users() ([]models.User,[]error)
+    GetUser(id uint) (*models.User, []error)
+    GetUserByUsername(username string) (*models.User,[]error)
+	RegisterUser(user *models.User)(*models.User,[]error)
 	EditUser(user *models.User)(*models.User,[]error)
-	DeleteUser(id uint)(*models.User,error)
+	DeleteUser(id uint)(*models.User,[]error)
+	UsernameExists(username string) bool
+
+}
+
+
+// SessionRepository specifies logged in user session related database operations
+type SessionRepository interface {
+	Session(sessionID string) (*models.Session, []error)
+	StoreSession(session *models.Session) (*models.Session, []error)
+	DeleteSession(sessionID string) (*models.Session, []error)
 }
