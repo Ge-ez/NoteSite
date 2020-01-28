203c1300a3933fbaf2b47859306d10e806140f8c
manny-e1 <52893548+manny-e1@users.noreply.github.com>
2020-01-28 23:58:34 +0300

    Update usermodel.go

--- models/usermodel.go
+++ models/usermodel.go
@@ -4,19 +4,16 @@ import "time"
 
 type User struct {
 	ID       uint32
-	Name     string
-	Username string
-	Email    string
-	Password string
-	Gender   string
-	Role     string
-	Course   string
-	Image    string
-	About    string
-	Token    string
+	Name     string `gorm:"type:varchar(255);not null"`
+	Username string `gorm:"type:varchar(255);not null, unique"`
+	Email    string	`gorm:"type:varchar(255);not null"`
+	Password string	`gorm:"type:varchar(255);not null"`
+	Gender   string	`gorm:"type:varchar(255);not null"`
+	Role     string	`gorm:"type:varchar(255);not null"`
+	Course   string	`gorm:"type:varchar(255);not null"`
+	Image    string	`gorm:"type:varchar(255)"`
+	About    string	`gorm:"type:varchar(255)"`
 	Joindate time.Time
 }
 
-type Usercheck struct {
-	Username, Password string
-}
+
