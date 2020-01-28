ee0c97e34e28dbbf179c29b9762be38ccd87022f
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-28 16:28:22 +0300

    Delete and Update a Note

--- upload/repository/gorm_upload.go
+++ upload/repository/gorm_upload.go
@@ -39,3 +39,23 @@ func (upld *UploadRepositoryImpl) Note(id uint) (*models.Upload,[]error){
     	}
     	return &note, errs
 }
+
+func (upld *UploadRepositoryImpl) UpdateNote(update *models.Upload) (*models.Upload,[]error){
+    note := update
+    	errs := upld.conn.Save(note).GetErrors()
+    	if len(errs) > 0 {
+    		return nil, errs
+    	}
+    	return note, errs
+}
+func (upld *UploadRepositoryImpl) DeleteNote(id uint) (*models.Upload,[]error){
+    note, errs := upld.Course(id)
+    	if len(errs) > 0 {
+    		return nil, errs
+    	}
+    	errs = upld.conn.Delete(note, id).GetErrors()
+    	if len(errs) > 0 {
+    		return nil, errs
+    	}
+    	return note, errs
+}
