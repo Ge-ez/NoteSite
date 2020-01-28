55108e8643eb62a66e5c6fa90265e0b63548d235
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-28 15:21:33 +0300

    UploadNoteHandler Function Created

--- Handlers/uploadHandler.go
+++ Handlers/uploadHandler.go
@@ -54,3 +54,70 @@ func (upldhand *NewUploadHandler) UploadedNotesHandler(w http.ResponseWriter, r 
 	}
 	upldhand.tmpl.ExecuteTemplate(w, "homepage.layout", tmplData)
 }
+
+func (upldhand *NewUploadHandler) UploadNoteHandler(w http.ResponseWriter, r *http.Request) {
+exts := []string{".pdf", ".docx", ".sys", ".txt", ".doc", ".ppt" , ".pptx",".zip"}
+	token, err := rtoken.CSRFToken(upldhand.csrfSignKey)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+	if r.Method == http.MethodGet {
+		uploadForm := struct {
+			Values  url.Values
+			VErrors form.ValidationErrors
+			CSRF    string
+		}{
+			Values:  nil,
+			VErrors: nil,
+			CSRF:    token,
+		}
+		upldhand.tmpl.ExecuteTemplate(w, "uploadNote.layout", uploadForm)
+	}
+
+	if r.Method == http.MethodPost {
+		// Parse the form data
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+		// Validate the form contents
+		uploadForm := form.Input{Values: r.PostForm, VErrors: form.ValidationErrors{}}
+		uploadForm.Required("title", "description")
+		uploadForm.MinLength("title",5)
+		uploadForm.MinLength("description", 10)
+		uploadForm.CSRF = token
+		// If there are any errors, redisplay the signup form.
+		if !uploadForm.Valid() {
+			upldhand.tmpl.ExecuteTemplate(w, "uploadNote.layout", uploadForm)
+			return
+		}
+		mf, fh, err := r.FormFile("upload")
+		if err != nil {
+			uploadForm.VErrors.Add("upload", "File error")
+			upldhand.tmpl.ExecuteTemplate(w, "uploadNote.layout", uploadForm)
+			return
+		}
+		if !form.ExtensionChecker(fh.Filename,exts){
+		    uploadForm.VErrors.Add("upload", "You can only upload the listed File Types!")
+            upldhand.tmpl.ExecuteTemplate(w, "uploadNote.layout", uploadForm)
+   			return
+		}
+		defer mf.Close()
+		upld := &models.Upload{
+			File_Name:        r.FormValue("title"),
+			File_Description: r.FormValue("description"),
+			File: fh.Filename,
+			File_Uploader: upldhand.LoggedInUser.ID,
+			File_Uploaded_to : upldhand.LoggedInUser.Course,
+			File_Type: FilePath.Ext(fh.Filename),
+			File:Uploaded_On: time.Now(),
+		}
+		UploadFile(&mf, fh.Filename)
+		_, errs := upldhand.uploadService.StoreNotes(upld)
+		if len(errs) > 0 {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		http.Redirect(w, r, "/homepage", http.StatusSeeOther)
+	}
+}
