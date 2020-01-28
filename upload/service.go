96bb0628d867fd6da969b5065e56babc8b3e11e9
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-28 16:13:25 +0300

    Interface UploadService Created

--- upload/service.go
+++ upload/service.go
@@ -1,1 +1,13 @@
 package upload
+
+import "github.com/notesite/models"
+
+type UploadService interface{
+     Notes() (*models.Upload,[]error)
+     Note(id uint) (*models.Upload,[]error)
+     UpdateNote(note *models.Upload) (*models.Upload,[]error)
+     DeleteNote(id uint) (*models.Upload,[]error)
+     NotesByStatus(status string) (*models.Upload,[]error)
+     NotesByUploader(id uint) (*models.Upload,[]error)
+     NotesByCourseName(course string) (*models.Upload,[]error)
+ }
