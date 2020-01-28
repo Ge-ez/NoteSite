e1eba678b6bfffd615299f39f2d124513647c44a
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-28 15:13:25 +0300

    Upload Handler Struct Created

--- Handlers/uploadHandler.go
+++ Handlers/uploadHandler.go
@@ -1,1 +1,25 @@
 package handlers
+
+import(
+   "html/template"
+    "net/http"
+    "net/url"
+    "time"
+    "io"
+   	"mime/multipart"
+   	"os"
+   	"path/filepath"
+   	"strconv"
+
+     "github.com/notesite/form"
+     "github.com/notesite/models"
+     "github.com/notesite/upload"
+     )
+
+
+type UploadHandler struct {
+	tmpl           *template.Template
+	uploadService    upload.UploadService
+	loggedInUser   *models.User
+	csrfSignKey []byte
+}
